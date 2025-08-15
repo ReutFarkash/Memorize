afb2a04c482398acd18262beb8e2b016b8346d76
Reut Farkash <reutfarkash@gmail.com>
2025-08-15 13:13:17 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Reut Farkash on 15/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
